52cef916fc6d2e3bd570b9c7ff4735232a9a8ab4
andyjmurph <andyjmurph@icloud.com>
2016-12-15 23:21:33 +0000

    fixing next and prev arrows on lightbox

--- js/app.js
+++ js/app.js
@@ -1,122 +1,101 @@
-//Search box
-  //Problem: User can type in the search box, but nothing happens thereafter
-  //Solution: Filter the gallery images as you type in the search box
+//Variables
+const searchBar = document.getElementById('search');
+const photoGallery = document.getElementById('gallery');
+const galleryItems = photoGallery.children;
+const overlayDiv = document.createElement('div');
+let overlayImage = document.createElement('img');
+let overlayCaption = document.createElement('p');
+const overlayCloseButton = document.createElement('i');
+const overlayLeftButton = document.createElement('i');
+const overlayRightButton = document.createElement('i');
+const endOfBody = document.querySelector('body script');
+let imageRef = '';
+let imageCaption = '';
 
-var searchBox = document.getElementById("search");
-var gallery = document.getElementById("gallery");
-var overlay = document.createElement("div");
-var image = document.createElement("img");
-var caption = document.createElement("p");
-var navLeft = document.createElement("i");
-var navRight = document.createElement("i");
-var close = document.createElement("i");
-var endBody = document.querySelector("body script");
-var currentListItem = "";
-var linkRef = "";
 
-var search = function() {
-  //Take the value of #search
-  searchValue = searchBox.value;
-  //Cycle through the .gallery-item(s)
-  for (i = 0; i < gallery.children.length; i++) {
-    //Get the .img-caption text
-    var galleryItem = gallery.getElementsByTagName("li")[i];
-    var caption = galleryItem.children[1].innerHTML;
-    //Compare the #search value with the .img-caption text
-    if (caption.indexOf(searchValue) > -1) {
-      //remove the class hidden
-      galleryItem.className = "gallery-item match";
+//Search Bar
+searchBar.addEventListener('keyup', (event) => {
+  searchBarValue = searchBar.value;
+  for ( let i = 0 ; i < galleryItems.length ; i += 1 ) {
+    let galleryItemCaption = galleryItems[i].lastElementChild.textContent;
+    if (galleryItemCaption.indexOf(searchBarValue) > -1 ) {
+      galleryItems[i].classList.remove('hidden');
+      galleryItems[i].classList.add('visible');
     } else {
-      //add the class hidden
-      galleryItem.className = "gallery-item hidden";
+        galleryItems[i].classList.remove('visible');
+        galleryItems[i].classList.add('hidden');
     }
   }
-}
+});
 
-//add overlay
-function addOverlay(event) {
-  //prevent default behaviour
+//add the overlay and show the selected image and caption
+photoGallery.addEventListener('click', (event) => {
   event.preventDefault();
-  //make overlay visible
-  overlay.style.display = "block";
-  //set selected li
-  currentListItem = event.currentTarget;
-  //get the src attribute of the image and the p text for the clicked li
-  linkRef = this.firstElementChild.getAttribute("href");
-  captionText = this.lastElementChild.innerHTML;
-  //update img src and caption text
-  image.setAttribute("src", linkRef);
-  caption.innerHTML = captionText;
-}
+  overlayDiv.style.display = 'block';
+  if (event.target.tagName == 'IMG') {
+    imageRef = event.target.parentNode.getAttribute('href');
+    imageCaption = event.target.parentNode.nextElementSibling.textContent;
+    selectedListItem = event.target.parentNode.parentNode;
+  } else if (event.target.tagName == 'P') {
+    imageRef = event.target.previousElementSibling.getAttribute('href');
+    imageCaption = event.target.textContent;
+    selectedListItem.event.target.parentNode;
+  }
+  updateOverlay();
+});
 
+//Cycle through the next image and caption whenc clicking on the next arrow
+overlayRightButton.addEventListener('click', () => {
+  while (selectedListItem.nextElementSibling != null) {
+    if (selectedListItem.nextElementSibling.classList.contains('visible') == false ) {
+      selectedListItem = selectedListItem.nextElementSibling;
+    } else {
+      break;
+    }
+  }
+  if ( selectedListItem.nextElementSibling != null && selectedListItem.nextElementSibling.classList.contains('visible')) {
+    selectedListItem = selectedListItem.nextElementSibling;
+    imageRef = selectedListItem.firstElementChild.getAttribute('href');
+    imageCaption = selectedListItem.lastElementChild.textContent;
+    updateOverlay();
+  }
+});
+
+//cycle through previous image and caption when clicking on the previous arrow
+overlayLeftButton.addEventListener('click', () => {
+  while (selectedListItem.previousElementSibling != null) {
+    if (selectedListItem.previousElementSibling.classList.contains('visible') == false ) {
+      selectedListItem = selectedListItem.previousElementSibling;
+    } else {
+      break;
+    }
+  }
+  if ( selectedListItem.previousElementSibling != null && selectedListItem.previousElementSibling.classList.contains('visible')) {
+    selectedListItem = selectedListItem.previousElementSibling;
+    imageRef = selectedListItem.firstElementChild.getAttribute('href');
+    imageCaption = selectedListItem.lastElementChild.textContent;
+    updateOverlay();
+  }
+});
+//update the image and caption on the overlay
 function updateOverlay() {
-  //set the image in the overlay's src attribute
-  image.setAttribute("src", linkRef);
-  //replace the caption with the captionText
-  caption.innerHTML = captionText;
+  overlayImage.setAttribute('src', imageRef);
+  overlayCaption.textContent = imageCaption;
 }
 
-function nextImage() {
-  nextListItem = currentListItem.nextElementSibling;
-  if (nextListItem != null) {
-    linkRef = nextListItem.firstElementChild.getAttribute("href");
-    captionText = nextListItem.lastElementChild.innerHTML;
-    updateOverlay();
-    currentListItem = nextListItem;
-  }
-}
+//Hide the overlay on clicking the overlayCloseButton
+overlayCloseButton.addEventListener('click', () => {
+  overlayDiv.style.display = 'none';
+});
 
-function prevImage() {
-  prevListItem = currentListItem.previousElementSibling;
-  if ( prevListItem != null ) {
-    linkRef = prevListItem.firstElementChild.getAttribute("href");
-    captionText = prevListItem.lastElementChild.innerHTML;
-    updateOverlay();
-    currentListItem = prevListItem;
-  }
-}
-
-//close overlay
-function closeOverlay() {
-  overlay.style.display = "none";
-}
-
-//Listen for click event on all li's
-for (i = 0; i < gallery.children.length; i ++) {
-  gallery.children[i].addEventListener("click", addOverlay);
-}
-
-//Listen for key up event on search box
-searchBox.addEventListener("keyup", search);
-
-//Listen for click event on close and prev buttons
-navRight.addEventListener("click", nextImage);
-navLeft.addEventListener("click", prevImage);
-
-//Listen for click event on close button
-close.addEventListener("click", closeOverlay);
-
-//Lightbox gallery
-  //Problem:  When clicking on an image, you see the full screen link with no option to go back
-  //Solution: Display the clicked image in an overlay with next and back buttons, and a close button
-
-//Create the overlay
-document.body.insertBefore(overlay, endBody);
-//add overlay class to overlay
-overlay.className = "overlay";
-//append image to overlay
-overlay.appendChild(image);
-//append caption to overlay
-overlay.appendChild(caption);
-//append left navigation arrow to overlay
-overlay.appendChild(navLeft);
-//add font awesome class to navLeft
-navLeft.className = "fa fa-chevron-left";
-//append right navigation arrow to overlay
-overlay.appendChild(navRight);
-//add font awesome class to navRight
-navRight.className = "fa fa-chevron-right";
-//append close button to overlay
-overlay.appendChild(close);
-//add font awesome class to close
-close.className = "fa fa-times";
+//Add the overlay
+document.body.insertBefore(overlayDiv, endOfBody);
+overlayDiv.className = 'overlay';
+overlayDiv.appendChild(overlayImage);
+overlayDiv.appendChild(overlayCaption);
+overlayDiv.appendChild(overlayLeftButton);
+overlayLeftButton.className = 'fa fa-chevron-left';
+overlayDiv.appendChild(overlayRightButton);
+overlayRightButton.className = 'fa fa-chevron-right';
+overlayDiv.appendChild(overlayCloseButton);
+overlayCloseButton.className = 'fa fa-times';
